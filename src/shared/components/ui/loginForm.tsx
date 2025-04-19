0357bb87f90fad6cf82f83d997983cef8ca2ecd3
Younghoon <yhkang2003@gmail.com>
2025-04-20 01:02:13 +0900

    refactor: Google login button , signup prompt 삭제

--- src/shared/components/ui/loginForm.tsx
+++ src/shared/components/ui/loginForm.tsx
@@ -50,15 +50,6 @@ export function LoginForm({
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <Button variant="outline" className="w-full">
-                Login with Google
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
             </div>
           </form>
         </CardContent>

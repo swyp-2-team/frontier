f733574e8c53523777705e7062eed0a58e2d13db
Jinsu <injnamek@gmail.com>
2025-04-21 18:52:22 +0900

    refactor: HomePage에 있던 Link를 헤더로 옮기기

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,3 @@
-import { Link } from "react-router-dom";
-
 export default function HomePage() {
-  return (
-    <>
-      <div>Home Page</div>
-      <Link to={"/users/login"}>로그인하기</Link>
-    </>
-  );
+  return <></>;
 }
--- src/shared/components/Layouts/Layout.tsx
+++ src/shared/components/Layouts/Layout.tsx
@@ -1,9 +1,10 @@
+import Header from "@/shared/components/Layouts/Header";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <header className="h-20 bg-amber-200">Header</header>
+      <Header></Header>
       <Outlet />
       <footer className="h-20 bg-amber-200">Footer</footer>
     </>

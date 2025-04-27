a3edcea3e9b0cb368e87897c226bd08c8fe52316
Younghoon <yhkang2003@gmail.com>
2025-04-27 17:25:56 +0900

    feat: 로그인 폼 개선 및 상태 유지 기능 추가

--- src/shared/components/ui/loginForm.tsx
+++ src/shared/components/ui/loginForm.tsx
@@ -1,15 +1,14 @@
+import { useState } from "react";
 import { cn } from "@shared/lib/utils";
 import { Button } from "@shared/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { useLogin } from "@/features/auth/model/useLogin";
+
+//import circleSpread from "@/assets/icons/circle_spread.svg";
+//import eyeOn from "@/assets/icons/eyes_on.svg";
+//import eyeoff from "@/assets/icons/eyes_off.svg";
 
 export function LoginForm({
   className,
@@ -18,49 +17,76 @@ export function LoginForm({
   // 로그인 훅
   const { onLogin, onChangeInput } = useLogin();
 
+  // 로그인 상태 유지 체크 상태
+  // 비밀번호 보이기 토글 상태
+
+  const [state, setState] = useState({
+    isLoginChecked: false,
+    pwIconChecked: false,
+  });
+
+  // 토글 함수
+  const toggleState = (key: keyof typeof state) => {
+    setState((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
+
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div
+      className={cn("flex flex-col w-full max-w-lg p-6", className)}
+      {...props}
+    >
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
+          <CardTitle>Log-in</CardTitle>
         </CardHeader>
         <CardContent>
           <form onSubmit={onLogin}>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
+            <div className="flex flex-col">
+              <div className="grid gap-1.5 mb-3">
+                <Label htmlFor="id">ID</Label>
                 <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="m@example.com"
+                  id="id"
+                  name="id"
+                  type="text"
+                  placeholder="아이디를 입력해주세요"
                   onChange={onChangeInput}
                   required
                 />
               </div>
-              <div className="grid gap-2">
+              <div className="grid gap-1.5 mb-5">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
                 </div>
                 <Input
                   id="password"
                   name="password"
-                  onChange={onChangeInput}
                   type="password"
+                  placeholder="비밀번호를 입력해주세요"
+                  onChange={onChangeInput}
                   required
                 />
+                <div className="flex justify-end"></div>
               </div>
-              <Button type="submit" className="w-full">
-                Login
+              <div className="pl-2.5 flex items-start">
+                <div
+                  className="flex items-center gap-2 cursor-pointer"
+                  onClick={() => toggleState("isLoginChecked")}
+                >
+                  <div className="text-right justify-start body-16 leading-normal">
+                    로그인 상태 유지
+                  </div>
+                </div>
+              </div>
+              <Button
+                type="submit"
+                className="w-full my-10 body-16_SB"
+                onClick={(e) => {
+                  // 폼 제출 시 isLoginChecked 값 사용
+                  console.log("로그인 상태 유지:", state.isLoginChecked);
+                  console.log("비밀번호 보이기 상태:", state.pwIconChecked);
+                }}
+              >
+                로그인
               </Button>
             </div>
           </form>

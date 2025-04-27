6348893ca3c1ac5c23b57accd157e10f635ac00d
Younghoon <yhkang2003@gmail.com>
2025-04-27 17:25:43 +0900

    feat: platform image를 png 형태로 추가

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,8 +2,17 @@ import { LoginForm } from "@/shared/components/ui/loginForm";
 
 export default function LoginPage() {
   return (
-    <div>
-      <LoginForm />
+    <div className="flex h-screen bg-gray-100 overflow-hidden">
+      <div className="flex-1 h-full">
+        <img
+          src="/src/assets/images/logIn.png"
+          alt="Login"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="flex-2 flex place-items-center justify-center">
+        <LoginForm />
+      </div>
     </div>
   );
 }

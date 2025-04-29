1a7875cb49765074ac8abfbeb63cd7e3ea898778
Jinsu <injnamek@gmail.com>
2025-04-29 12:14:03 +0900

    feat: add Profile component

--- src/shared/components/Layouts/Header.tsx
+++ src/shared/components/Layouts/Header.tsx
@@ -1,3 +1,9 @@
+import Profile from "@/features/user/ui/Profile";
+
 export default function Header() {
-  return <div className="h-20 border-b border-gray-400"></div>;
+  return (
+    <div className="flex px-[42px] items-center h-20 border-b border-gray-400">
+      <Profile />
+    </div>
+  );
 }
--- src/shared/components/ui/Banner.tsx
+++ src/shared/components/ui/Banner.tsx
@@ -8,13 +8,13 @@ export default function Banner() {
         <h2 className="headline-40 mb-6">
           장애를 손쉽게 등록하고, 실시간으로 전파하세요
         </h2>
-        <p className="title-20 mb-[30px]">
+        <p className="title-20_SB mb-[30px]">
           이메일부터 메신저까지, 각종 알림으로 장애 발생을 빠르게 알려요.
         </p>
         <Button
           asChild
           variant={"basic"}
-          className="bg-white text-black title-20_SB w-[214px] h-[50px] rounded-[12px] hover:bg-primary hover:text-white"
+          className="bg-white text-black body-18_SB w-[156px] h-[48px] rounded-[12px] hover:bg-primary hover:text-white py-[10px] px-[46px]"
         >
           <Link to={"/incident-register"}>장애등록</Link>
         </Button>

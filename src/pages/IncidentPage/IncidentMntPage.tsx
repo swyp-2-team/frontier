4b00ca13bcb6ae426998454c32ec0bdde6cc6f99
Younghoon <yhkang2003@gmail.com>
2025-05-03 02:29:10 +0900

    Revert "style: breadcrumb 적용"
    This reverts commit c7bb6d4c0fd6775f2da01e3a53f1a76d761b3c2b.

--- src/pages/IncidentPage/IncidentMntPage.tsx
+++ src/pages/IncidentPage/IncidentMntPage.tsx
@@ -3,35 +3,12 @@ import { useState } from "react";
 import IncidentList from "@/features/incident/ui/IncidentList";
 import ToggleSelect from "@/features/incident/ui/ToggleSelect";
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from "@shared/components/ui/breadcrumb";
-
 export default function IncidentMntPage() {
   const [showActive, setShowActive] = useState(true);
 
   return (
-    <div className="flex-1 flex-col w-full min-w-md justify-self-center border-l border-gray-400 h-screen px-[42px]">
-      <Breadcrumb className="self-start mb-[38px]">
-        <BreadcrumbList className="body-13 text-gray-800">
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/home">홈</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink
-              href="/incident-mnt"
-              className="body-13_SB text-black"
-            >
-              장애관리
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+    <div className="flex-1 flex-col w-full min-w-md justify-self-center gap-[26px] mt-[26px] px-[42px]">
+      <h1>홈 {`>`} 장애관리</h1>
       <div>
         <ToggleSelect
           activeTab={showActive ? "active" : "history"}

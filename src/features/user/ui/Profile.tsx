5aa03b5fd0299322c4f753c18a89452911dd7bd0
Younghoon <yhkang2003@gmail.com>
2025-05-02 21:56:13 +0900

    feat:  유저 댓글을 추가한IncidentMntDetailPage에 CommentSection 추가

--- src/features/user/ui/Profile.tsx
+++ src/features/user/ui/Profile.tsx
@@ -1,8 +1,16 @@
-export default function Profile() {
+interface ProfileProps {
+  username?: string;
+  className?: string;
+}
+
+export default function Profile({
+  username = "Noti-Core",
+  className = "",
+}: ProfileProps) {
   return (
-    <article className="flex items-center gap-3 ml-auto">
+    <article className={`flex items-center gap-3 ${className}`}>
       <div className="block size-8 bg-gray-400 rounded-full"></div>
-      <span className="body-16">Noti-Core</span>
+      <span className="body-16">{username}</span>
     </article>
   );
 }

9697f0c279879c070b58661f0f504e5af591bac7
Jinsu <injnamek@gmail.com>
2025-05-15 16:40:14 +0900

    style: 로그아웃 버튼 Figma 시안에 맞게 디자인 변경

--- src/features/auth/ui/logoutButton.tsx
+++ src/features/auth/ui/logoutButton.tsx
@@ -2,20 +2,16 @@ import { navigate } from "@/shared/lib/navigation";
 
 export default function LogoutButton() {
   const handleLogout = () => {
-    console.log("Logging out");
+    console.log("Log out");
     navigate("/");
   };
 
   return (
-    <div className="w-20 p-2.5 inline-flex flex-col justify-start items-start gap-2.5">
-      <button
-        onClick={handleLogout}
-        className="self-stretch h-6 px-1.5 py-1 bg-gray-300 rounded-lg inline-flex justify-center items-center"
-      >
-        <div className="body-13 justify-start text-gray-900 leading-none">
-          로그아웃
-        </div>
-      </button>
-    </div>
+    <button
+      onClick={handleLogout}
+      className="px-2 py-1 bg-gray-300 rounded-lg body-14 text-gray-600"
+    >
+      로그아웃
+    </button>
   );
 }

9101779cba25a5da6e7e13d541641bc08122b52f
Jinsu <injnamek@gmail.com>
2025-05-15 16:31:27 +0900

    fix: 로그인 페이지에서 navigation 함수가 실행되지 않는 문제 해결

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,15 @@
 import { LoginForm } from "@/features/auth/ui/loginForm";
+import { setNavigator } from "@/shared/lib/navigation";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const nav = useNavigate();
+
+  useEffect(() => {
+    setNavigator(nav);
+  }, [nav]);
+
   return (
     <div className="flex h-screen bg-gray-100 overflow-hidden">
       <div className="flex-1 h-full max-w-[716px]">

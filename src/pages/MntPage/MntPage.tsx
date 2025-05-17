6f15df281c62ccda93adea3504375362b275cdaf
Younghoon <yhkang2003@gmail.com>
2025-05-18 07:16:30 +0900

    Hotfix: 사용자 관리 페이지 문구 변경

--- src/pages/MntPage/MntPage.tsx
+++ src/pages/MntPage/MntPage.tsx
@@ -1,3 +1,7 @@
 export default function MntPage() {
-  return <div>관리 페이지</div>;
+  return (
+    <div className="flex flex-col items-center justify-center px-10 body-16_SB text-gray-800">
+      고도화 예정입니다.
+    </div>
+  );
 }

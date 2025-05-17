4832384a7e536570c3dc23c1b14deeb79e06a1b9
Younghoon <yhkang2003@gmail.com>
2025-05-18 07:18:00 +0900

    hotfix: 폰트 크기 키움

--- src/pages/MntPage/MntPage.tsx
+++ src/pages/MntPage/MntPage.tsx
@@ -1,6 +1,6 @@
 export default function MntPage() {
   return (
-    <div className="flex flex-col items-center justify-center px-10 body-16_SB text-gray-800">
+    <div className="flex flex-col items-center justify-center px-10 headline-48 text-gray-800">
       고도화 예정입니다.
     </div>
   );

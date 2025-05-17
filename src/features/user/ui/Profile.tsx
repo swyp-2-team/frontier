5d4cf4d14688b20d4533f478e9b207ddebaf5f43
Younghoon <yhkang2003@gmail.com>
2025-05-18 07:27:32 +0900

    hotfix: localstorage 통해서 username 저장

--- src/features/user/ui/Profile.tsx
+++ src/features/user/ui/Profile.tsx
@@ -1,18 +1,15 @@
 import { cn } from "@/shared/lib/utils";
 
 interface ProfileProps {
-  username?: string;
   className?: string;
 }
 
-export default function Profile({
-  username = "NotiCore",
-  className = "",
-}: ProfileProps) {
+export default function Profile({ className = "" }: ProfileProps) {
+  const name = localStorage.getItem("userInfo") || "NotiCore";
   return (
     <article className={cn("flex items-center gap-3 ml-auto", className)}>
       <div className="block size-8 bg-gray-400 rounded-full"></div>
-      <span className="body-16">{username}</span>
+      <span className="body-16">{name}</span>
     </article>
   );
 }

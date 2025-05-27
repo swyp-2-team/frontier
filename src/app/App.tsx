ca18c24283a5aedf784a80d10cdbf42448fa0713
Younghoon <yhkang2003@gmail.com>
2025-05-28 02:22:08 +0900

    feat: Auth Provider 앱 루트에 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,12 @@ import { Toaster } from "@shared/components/ui/sonner";
 import { useBreakpoint } from "@/shared/lib/useBreakpoint";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router/routes.tsx";
+import { AuthProvider } from "@/features/auth/context/AuthContext";
 
 function App() {
   const breakpoint = useBreakpoint();
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
       <Toaster
         richColors
@@ -15,7 +16,7 @@ function App() {
           right: breakpoint === "2xl" ? "300px" : "208px",
         }}
       />
-    </>
+    </AuthProvider>
   );
 }
 
